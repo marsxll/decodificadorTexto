48c6c278a234fe5f0a3e36bb92869386d17d91e2
marsxll <114643548+marsxll@users.noreply.github.com>
2024-07-08 20:11:47 -0300

    Add files via upload

---
+++ assets/script/script.js
@@ -0,0 +1,53 @@
+const areaResponse = document.querySelector('.areaResponse')
+const btnEncrypt = document.querySelector('#btnEncrypt');
+const btnDecrypt = document.querySelector('#btnDecrypt');
+const btnCopy = document.querySelector('#btnCopy')
+const txtResponse = document.querySelector('.txtResponse');
+const txtArea = document.querySelector('.txtArea');
+
+btnEncrypt.addEventListener('click', encrypt);
+btnDecrypt.addEventListener('click', decrypt);
+btnCopy.addEventListener('click', copy);
+
+function encrypt() {
+    if( txtArea.value.length > 0) {
+        const txtEncypt = txtArea.value;
+        const response = txtEncypt.replaceAll('e', 'enter')
+                                .replaceAll('i', 'imes')
+                                .replaceAll('a','ai')
+                                .replaceAll('o','ober')
+                                .replaceAll('u','ufat');
+        txtResponse.innerHTML = response;
+        txtArea.value = "";
+        btnCopy.style.display = "block";
+        areaResponse.classList.add('response');
+        return response;
+    }
+}
+
+function decrypt() {
+    if (txtArea.value.length > 0) {
+        const txtDncypt = txtArea.value;
+        const response = txtDncypt.replaceAll('enter', 'e')
+                                    .replaceAll('imes', 'i')
+                                    .replaceAll('ai','a')
+                                    .replaceAll('ober','o')
+                                    .replaceAll('ufat','u');
+        txtResponse.innerHTML = response;
+        txtArea.value = "";
+        btnCopy.style.display = "block";
+        areaResponse.classList.add('response');
+        return response;
+    }
+}
+
+function copy() {    
+    const textArea = document.createElement("textarea");
+    textArea.value = txtResponse.innerText;
+    document.body.appendChild(textArea);
+    textArea.select();
+    document.execCommand("copy");
+    document.body.removeChild(textArea);
+    alert("Texto Copiado!");
+    
+}
